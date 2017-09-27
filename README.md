7ea2a71c0d87a32eba940a495c13cb8c4d173392
Keith Packard <keithp@keithp.com>
2017-09-27 15:49:04 -0700

    Add setup script and readme
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+mem-reserve computes kernel parameters needed to reserve the specified
+amount of memory for uses other than regular system memory.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='mem-reserve',
+      version='1.0',
+      description='Compute memory reservation parameters',
+      author='Keith Packard',
+      author_email='keithp@keithp.com',
+      url='https://github.com/FabricAttachedMemory/mem-reserve',
+      scripts=['mem-reserve']
+     )
